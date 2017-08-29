762620f8fc5acf9ce43d4dfc501966b54d014ada
saki <saki@ginuuk.net>
2017-08-29 22:20:12 +0900

    meet/index --> view のリンク間違いを修正。html エスケープ忘れ箇所を修正。

--- application/views/meets/view.php
+++ application/views/meets/view.php
@@ -5,8 +5,8 @@
 		会場：<?= h($meet['location']) ?></br>
 		主催：<?= h($meet['organized_by']) ?></br>
 		大会HP：<a href="<?= $meet['homepage']; ?>"><?= h($meet['homepage']) ?></a></br>
-		シーズン：<a href="<?= site_url('seasons/' . $meet['season_id']); ?>"><?= h($meet['ss_name']) ?></a></br>
-		シリーズ：<a href="<?= site_url('meet_groups/' . $meet['meet_group_code']); ?>"><?= h($meet['mg_name']) ?></a></br>
+		シーズン：<a href="<?= site_url('seasons/' . h($meet['season_id'])); ?>"><?= h($meet['ss_name']) ?></a></br>
+		シリーズ：<a href="<?= site_url('meet_groups/' . h($meet['meet_group_code'])); ?>"><?= h($meet['mg_name']) ?></a></br>
 	</p>
 	<h3>レース</h3>
 	<?php if (empty($entries)): ?>
@@ -25,10 +25,10 @@
 			<?php foreach ($entries as $e): ?>
 				<tr>
 					<td><?= h($e['start_clock']) ?></td>
-					<td><a href ="<?= site_url('race/' . $e['ec_id']) ?>"><?= h($e['ec_name']) ?></a></td>
+					<td><a href ="<?= site_url('race/' . h($e['ec_id'])) ?>"><?= h($e['ec_name']) ?></a></td>
 					<td><?= h($e['races_category_code']) ?></td>
 					<td>
-						<a href ="<?= site_url('racer/' . $e['racer_code']) ?>">
+						<a href ="<?= site_url('racer/' . h($e['racer_code'])) ?>">
 						<?= h($e['name_at_race']) ?>
 						</a>
 					</td>
@@ -52,9 +52,9 @@
 		<tbody>
 			<?php foreach ($rank_ups as $r): ?>
 				<tr>
-					<td><a href ="<?= site_url('racer/' . $r['racer_code']) ?>"><?= h($r['racer_code']) . '／' . h($r['name_at_race']) ?></a></td>
+					<td><a href ="<?= site_url('racer/' . h($r['racer_code'])) ?>"><?= h($r['racer_code']) . '／' . h($r['name_at_race']) ?></a></td>
 					<td><?= h($r['team_name']) ?></td>
-					<td><a href ="<?= site_url('race/' . $r['entry_category_id']) ?>"><?= h($r['ec_name']) ?></a></td>
+					<td><a href ="<?= site_url('race/' . h($r['entry_category_id'])) ?>"><?= h($r['ec_name']) ?></a></td>
 					<td><?= h($r['cr_cat_code']) ?></td>
 				</tr>
 			<?php endforeach; ?>
