84eacf8f2b3965a15fea17084afbaafc24d333eb
saki <saki@ginuuk.net>
2018-04-08 00:44:51 +0900

    .animate() を使用するため、jquery を slim version からノーマル version に変更。

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,7 +8,11 @@
 		<link href="<?= base_url('assets/css/main.css'); ?>" rel="stylesheet">
 		<link href="<?= base_url('assets/css/owl.carousel.css'); ?>" rel="stylesheet">
 		
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script
+			  src="https://code.jquery.com/jquery-3.3.1.min.js"
+			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+			  crossorigin="anonymous">
+		</script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 		<script src="<?= base_url('assets/js/bootstrap.min.js'); ?>"></script>
 		<script src="<?= base_url('assets/js/owl.carousel.js'); ?>"></script>
