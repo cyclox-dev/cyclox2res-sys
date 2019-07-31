1524c443451d8f900ef75dcf10450ed0e5523add
saki <saki@ginuuk.net>
2019-08-01 01:07:31 +0900

    DNQ --> DSQ に変更

--- application/etc/cyclox/Const/RacerResultStatus.php
+++ application/etc/cyclox/Const/RacerResultStatus.php
@@ -12,7 +12,7 @@ class RacerResultStatus
 	public static $DNS;
     public static $FIN;
     public static $DNF;
-    public static $DNQ;
+    public static $DSQ;
     public static $LAPOUT;
     public static $LAPOUT80;
 	
@@ -28,7 +28,7 @@ class RacerResultStatus
 		self::$DNS = new RacerResultStatus(0, '出走せず', 'DNS', 0, false, 'DNS');
 		self::$FIN = new RacerResultStatus(1, 'ゴール到達', 'FIN', 100, true, 'FIN');
 		self::$DNF = new RacerResultStatus(2, 'ゴールできず', 'DNF', 30, false, 'DNF');
-		self::$DNQ = new RacerResultStatus(3, '失格(除外)', 'DNQ', 10, false, 'DSQ');
+		self::$DSQ = new RacerResultStatus(3, '失格(除外)', 'DSQ', 10, false, 'DSQ');
 		self::$LAPOUT = new RacerResultStatus(4, '周回遅れラップアウト', 'DNF(Lap-Out)', 60, true, 'LapOut');
 		self::$LAPOUT80 = new RacerResultStatus(5, '80%ラップアウト適用', 'DNF(80%Out)', 65, true, '80%Out');
 		
@@ -36,7 +36,7 @@ class RacerResultStatus
 			self::$DNS,
 			self::$FIN,
 			self::$DNF,
-			self::$DNQ,
+			self::$DSQ,
 			self::$LAPOUT,
 			self::$LAPOUT80
 		);
