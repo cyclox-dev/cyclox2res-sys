d45e2f69cb3d4a361eaa30a17ca16c09f1450f78
saki <saki@ginuuk.net>
2021-10-08 23:21:53 +0900

    パートナーロゴの追加

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -13,10 +13,11 @@
 		<li><a href="https://ircbike.jp/" target="_blank"><img src="https://www.cyclocross.jp/img/partners/ptnr_logo_irc.svg" width="255" height="105" alt="IRC TIRE"></a></li>
 		<li><a href="https://www.canyon.com/ja/specials/takeflite/?utm_campaign=InfliteCFSLX-JP-Aug-2017&utm_medium=display&utm_source=cyclocross.jp&utm_content=&utm_term=" target="_blank"><img src="https://www.cyclocross.jp/img/partners/ptnr_logo_canyon.svg" width="255" height="105" alt="Canyon"></a></li>
 		<li><a href="https://www.giant.co.jp/" target="_blank"><img src="https://www.cyclocross.jp/img/partners/ptnr_logo_giant.svg" width="255" height="105" alt="GIANT"></a></li>
-		<li><a href="http://www.diatechproducts.com/muc-off/" target="_blank"><img src="https://www.cyclocross.jp/img/partners/ptnr_logo_muc-off.jpg" width="255" height="105" alt="Muc-Off"></a></li>
+		<li><a href="https://www.diatechproducts.com/muc-off/" target="_blank"><img src="https://www.cyclocross.jp/img/partners/ptnr_logo_muc-off.jpg" width="255" height="105" alt="Muc-Off"></a></li>
+		<li><a href="https://www.iris21.jp/" target="_blank"><img src="https://www.cyclocross.jp/img/partners/ptnr_logo_exlub.svg" width="255" height="105" alt="EXLUB"></a></li>
 		<li><a href="https://tsss.co.jp/" target="_blank"><img src="https://www.cyclocross.jp/img/partners/ptnr_logo_tsss.jpg" width="255" height="105" alt="OnebyESU"></a></li>
 		<li><a href="https://www.trekbikes.co.jp/" target="_blank"><img src="https://www.cyclocross.jp/img/partners/ptnr_logo_trek.svg" width="255" height="105" alt="TREK"></a></li>
-    </ul>
+	</ul>
 </div>
 <!-- /PARTNERS -->
 
